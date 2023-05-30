858a9d434dfd19c32019084b7dc999ed43ba90b0
Matthijs Veldkamp <112868719+MatthijsVeldkamp@users.noreply.github.com>
2023-05-30 08:59:03 +0200

    d

--- HTML form/welcome.php
+++ HTML form/welcome.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
         else
         {
             $naam = "";
-            $nameErr = "Nice try buddy";
+            $nameErr = "Only letters and white space allowed";
         }
        
         
